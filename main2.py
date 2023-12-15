7be729db44bc6e38aa799845f3a1051e1c76f0a6
ArefaSatellite <aivanov@up2u.pro>
2023-12-15 21:28:24 +0300

    first commit

---
+++ main2.py
@@ -0,0 +1,59 @@
+import base64
+import datetime
+from io import BytesIO
+
+from flask import Flask, render_template, request, url_for
+import numpy as np
+from matplotlib import pyplot as plt
+from decoder import Tibox
+
+app = Flask(__name__)
+
+t = Tibox()
+
+@app.route("/", methods=['POST', 'GET'])
+def main_page():
+    # if request.method == 'POST':
+    #     a = float(request.form.get('a'))
+    #     b = float(request.form.get('b'))
+    #     return render_template("home.html", a=a, b=b, answer=a + b)
+    lessons = [lesson.name for lesson in t.get_lessons()]
+    if len(lessons) == 0:
+        lessons = ["А их неть! (^_^)"]
+    return render_template('home.html', lessons_tip=lessons)\
+
+
+@app.route("/aboutus", methods=['POST', 'GET'])
+def aboutus():
+    return render_template('aboutus.html')
+
+
+@app.route("/result", methods=['POST'])
+def result():
+    points = []
+    print(request.form)
+    dt_start = datetime.datetime.strptime(request.form.get('start_date'), "%Y-%m-%d")
+    orderBy = request.form.get('OrderBy')
+
+    stat = t.make_stat(dt_start, orderBy)
+    values = list(stat.values())
+    keys = list(map(lambda x: x.split()[0], list(stat)))
+
+    fig, ax = plt.subplots()
+    if orderBy == Tibox.OrderBy.TEACHERS:
+        ax.bar(keys, values)
+    elif orderBy == Tibox.OrderBy.DAYS:
+        ax.hist(keys, bins=len(keys), weights=values)
+
+    ax.set_xticks(keys)
+    ax.set_xticklabels(keys, rotation=-45, fontsize=6)
+    ax.grid()
+    buf = BytesIO()
+    fig.savefig(buf, format='png')
+    data = base64.b64encode(buf.getbuffer()).decode('ascii')
+
+    return render_template("result.html", list_stat=keys, stat=stat, picture=data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
